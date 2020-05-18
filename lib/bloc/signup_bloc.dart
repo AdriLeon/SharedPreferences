6a6e32ddd9e728a6ff5142d6a5bc1c2d44b79ecd
AdriLeon <51243138+AdriLeon@users.noreply.github.com>
2020-05-18 01:35:39 -0500

    Delete signup_bloc.dart

--- lib/bloc/signup_bloc.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:async';
-
-class LoginBloc{
-
-
-  final _emailController = StreamController<String>.broadcast();
-  final _passwordController = StreamController<String>.broadcast();
-  final _userController = StreamController<String>.broadcast();
-}
